8774021f4b4ef9142972d6b9ff045e22cc741801
Cornell Bethea <betheacornellb@gmail.com>
2022-11-28 14:31:43 -0500

    searchable

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,29 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value 
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node1)
+        self.parent.children.delete(self) if @parent != nil
+        @parent = node1 
+        if node1 != nil && !node1.children.include?(self) 
+            node1.children << self
+        end
+
+    end
+
+    def add_child(node)
+        node.parent=(self)
+
+    end
+      
+    def remove_child(node)
+        node.parent=(nil)
+        raise if !self.children.include?(node)
+    end
 end
