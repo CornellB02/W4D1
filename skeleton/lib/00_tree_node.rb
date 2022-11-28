dc882c6a41dca80f000355368f03ceb88659dbe7
Cornell Bethea <betheacornellb@gmail.com>
2022-11-28 15:29:31 -0500

    bfs

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -25,5 +25,29 @@ class PolyTreeNode
     def remove_child(node)
         node.parent=(nil)
         raise if !self.children.include?(node)
+    end   
+
+    def dfs(target)
+        return self if self.value == target 
+        return nil if self.children.empty?
+
+        self.children.each do |child|
+            call = child.dfs(target)
+            if !call.nil?
+                return call
+            end
+        end
+        nil
     end
+
+    def bfs(target)
+        arr = [self]
+
+        return self if @value == target 
+        self.children.each do |child|
+            
+        end
+    end
+
+
 end
